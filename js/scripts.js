7ef8f81c563930499dc62da70017140480875534
Maggie S <Maggiesaldivia@gmail.com>
2021-02-10 22:47:23 -0800

    make submit button and user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,28 @@
-
-
-
 //business logic
 
 function encode(str) {
-  const normalizedString = str.replace(/[\W]/gi, "")
-	
+  const normalizedStr = str.replace(/[^\w]/gi, "").toLowerCase();
+  const strLength = normalizedStr.length;
+  const columns = Math.ceil(Math.sqrt(strLength));
+
+  let encodedText = "";
+
+  for (let i = 0; i < columns; i ++) {
+    for (let j = i; j < strLength; j += columns) {
+      encodedText += normalizedStr[j];
+    }
+    encodedText += '\n';
+  }
+  return encodedText
+}
+
+// user interface logic
+$(document).ready(function() {
+  $("form#word").submit(function(event) {
+    const userMessage = $("input#message").val();
+    $("#output").show()
+    $("#output").text("Enter the Matrix: \n" + encode(userMessage));
+    $("form#word").hide();
+    event.preventDefault();
+  })
+});
